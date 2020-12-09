2bf28b9198b81f7b05a30cfd9d0b267b8146f91f
Harjit Mavi <mavi.harjit@gmail.com>
2020-12-08 20:57:34 -0700

    Trying to commit from within vscode

--- hello_flutter_app/lib/main.dart
+++ hello_flutter_app/lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Hello Flutter'),
+      home: MyHomePage(title: 'Hello Dear Flutter'),
     );
   }
 }
