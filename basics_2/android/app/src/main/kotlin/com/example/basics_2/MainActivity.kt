6e1a753e849b3feccefcb587612340a4c398c0d0
Harjit Mavi <mavi.harjit@gmail.com>
2020-12-09 17:11:04 -0700

    This one renders the text in app as expected.

---
+++ basics_2/android/app/src/main/kotlin/com/example/basics_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.basics_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ basics_2/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Text(
+    'This is my text',
+    textDirection: TextDirection.ltr,
+  ));
+}
