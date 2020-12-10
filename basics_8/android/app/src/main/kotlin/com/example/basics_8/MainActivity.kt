72dbaf9c898c6f9240d4994f7323c33c6d1e101b
Harjit Mavi <mavi.harjit@gmail.com>
2020-12-10 09:59:15 -0700

    expression function aka fat arrow and removing debug flag

---
+++ basics_8/android/app/src/main/kotlin/com/example/basics_8/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.basics_8
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ basics_8/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/basic.dart';
+
+//void main() {
+//  runApp(new MyClass()); // the new keyword is optional and usually skipped
+//} // main
+
+// the following expression function or fat arrow will also work since
+// there is only one statement within brackets
+void main() => runApp(MyClass());
+
+class MyClass extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "My Flutter app",
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('My Title'),
+          ),
+          body: Material(
+              color: Colors.yellow,
+              child: Center(
+                  child: Text(
+                'This is my text',
+                textDirection: TextDirection.ltr,
+              ))),
+        ));
+  } // Widget
+
+} // MyClass
