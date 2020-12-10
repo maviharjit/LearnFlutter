34fd162127f0668abeb396d3c409e7d7ececc0cb
Harjit Mavi <mavi.harjit@gmail.com>
2020-12-09 17:29:15 -0700

    introducing the MaterialApp

---
+++ basics_3/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+        title: "this is my flutter app",
+        home: Text(
+          'This is my text',
+          textDirection: TextDirection.ltr,
+        )),
+  ); // runApp
+} // main
