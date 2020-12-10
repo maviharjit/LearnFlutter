8117ead6b638574289ea46ac1d6bd7a9a1fd72ae
Harjit Mavi <mavi.harjit@gmail.com>
2020-12-09 20:24:05 -0700

    backgroundColor within Scaffold

---
+++ basics_5/android/app/src/main/kotlin/learnflutterpackage/basics_5/MainActivity.kt
@@ -0,0 +1,6 @@
+package learnflutterpackage.basics_5
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ basics_5/lib/main.dart
@@ -0,0 +1,19 @@
+//  import 'dart:web_gl';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+        title: "My Flutter app",
+        home: Scaffold(
+            backgroundColor: Colors.yellow,
+            appBar: AppBar(
+              title: Text('My Title'),
+            ),
+            body: Text(
+              'This is my text',
+              textDirection: TextDirection.ltr,
+            )) // Scaffold
+        ), // MaterialApp
+  ); // runApp
+} // main
