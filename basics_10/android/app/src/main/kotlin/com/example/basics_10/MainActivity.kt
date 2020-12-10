8ec390b61ae3d5ef7b4e97365758a798919560df
Harjit Mavi <mavi.harjit@gmail.com>
2020-12-10 11:06:59 -0700

    A function that returns a random number as an int

---
+++ basics_10/android/app/src/main/kotlin/com/example/basics_10/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.basics_10
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ basics_10/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/basic.dart';
+import 'package:basics_10/second_file.dart';
+
+void main() {
+  runApp(new MyClass()); // the new keyword is optional and usually skipped
+} // main
+
+class MyClass extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        // this removes the debug flag
+        debugShowCheckedModeBanner: false,
+        title: "My Flutter app",
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('My Title'),
+            ),
+            body: SecondClass()));
+  } // Widget
+
+} // MyClass
