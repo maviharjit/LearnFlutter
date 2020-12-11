f234995bde12778efb83d06e1795fffadbf008fb
Harjit Mavi <mavi.harjit@gmail.com>
2020-12-10 22:40:15 -0700

    Text, Row and Column with multiple containers, SingleChildScrollView

--- basics_15/lib/second_file.dart
+++ basics_15/lib/second_file.dart
@@ -6,22 +6,23 @@ class SecondClass extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      // This column contains two containers
-      child: Column(
-        // MainAxisAlignment options are .spceBetween, .spaceAround, .spaceEvenly, .center, .end, .start
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Container(
-            color: Colors.yellow,
-            height: 400,
-            width: 250,
-          ),
-          Container(
-            color: lightPurpleColor,
-            height: 400,
-            width: 250,
-          )
-        ]
+      // SingleChildScrollView scrolls the two Columns which are otherwise bigger than screen size
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+              color: Colors.yellow,
+              height: 400,
+              width: 250,
+            ),
+            Container(
+              color: lightPurpleColor,
+              height: 400,
+              width: 250,
+            )
+          ]
+        )
       )
     );
   } // Widget
