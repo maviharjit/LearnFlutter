dc904fa80e839c99036366006fa48635d813e096
Harjit Mavi <mavi.harjit@gmail.com>
2020-12-12 16:18:19 -0700

    navigating screens, push(), pop(), added a dialog to disable the back button from closing app

--- basics_20/lib/first_screen.dart
+++ basics_20/lib/first_screen.dart
@@ -6,121 +6,137 @@ class FirstPageClass extends StatelessWidget {
     // Make sure each sceeen/class has the same scaffold unless you want different titles on differnt pages
     return Scaffold(
       appBar: AppBar(
-      elevation: 10.0,
-      title: Center(child: Text('My Title'),),
-      actions: <Widget>[Icon(Icons.settings)],
-    ),
-        body: Material(
-            child: Column(
+        elevation: 10.0,
+        title: Center(
+          child: Text('My Title'),
+        ),
+        actions: <Widget>[Icon(Icons.settings)],
+      ),
+      body: Material(
+        // Warn the user that app will close if they pressed the back button
+        child: WillPopScope(
+          onWillPop: (){
+            showDialog(
+              context: context,
+              // what is this underscore about?
+              builder: (_)=> AlertDialog(
+                title: Text('Are you sure?'),
+                content: Text('It will close the applicatoin'),
+              ),
+            );
+          },
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: Container(
-                  color: Colors.purple,
-                  child: Text(
-                    "Expanded",
-                    style: TextStyle(fontSize: 25),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      color: Colors.purple,
+                      child: Text(
+                        "Expanded",
+                        style: TextStyle(fontSize: 25),
+                      ),
+                    ),
                   ),
-                ),
+                  Flexible(
+                    child: Container(
+                      color: Colors.yellow,
+                      child: Text(
+                        "Flexible",
+                        style: TextStyle(fontSize: 25),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              Flexible(
-                child: Container(
-                  color: Colors.yellow,
-                  child: Text(
-                    "Flexible",
-                    style: TextStyle(fontSize: 25),
+              Row(
+                children: <Widget>[
+                  Flexible(
+                    child: Container(
+                      color: Colors.yellow,
+                      child: Text(
+                        "Flexible",
+                        style: TextStyle(fontSize: 25),
+                      ),
+                    ),
                   ),
+                  Flexible(
+                    child: Container(
+                      color: Colors.yellow,
+                      child: Text(
+                        "Flexible",
+                        style: TextStyle(fontSize: 25),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      color: Colors.purple,
+                      child: Text(
+                        "Expanded",
+                        style: TextStyle(fontSize: 25),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      color: Colors.purple,
+                      child: Text(
+                        "Expanded",
+                        style: TextStyle(fontSize: 25),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.orange,
+                      child: Text(
+                        "Expanded flex: 1",
+                        style: TextStyle(fontSize: 25),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                      color: Colors.orange,
+                      child: Text(
+                        "Expanded flex: 3",
+                        style: TextStyle(fontSize: 25),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              // this takes you to screen 2
+              GestureDetector(
+                child: Padding(
+                  child: Text(
+                    'Click me for next page',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 20,
+                    ),
+                  ),
+                  padding: EdgeInsets.all(30),
                 ),
+                onTap: () {
+                  Navigator.of(context).pushNamed('/screen2');
+                },
               ),
             ],
           ),
-          Row(
-            children: <Widget>[
-              Flexible(
-                child: Container(
-                  color: Colors.yellow,
-                  child: Text(
-                    "Flexible",
-                    style: TextStyle(fontSize: 25),
-                  ),
-                ),
-              ),
-              Flexible(
-                child: Container(
-                  color: Colors.yellow,
-                  child: Text(
-                    "Flexible",
-                    style: TextStyle(fontSize: 25),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: Container(
-                  color: Colors.purple,
-                  child: Text(
-                    "Expanded",
-                    style: TextStyle(fontSize: 25),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Colors.purple,
-                  child: Text(
-                    "Expanded",
-                    style: TextStyle(fontSize: 25),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              Expanded(
-                flex: 1,
-                child: Container(
-                  color: Colors.orange,
-                  child: Text(
-                    "Expanded flex: 1",
-                    style: TextStyle(fontSize: 25),
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 3,
-                child: Container(
-                  color: Colors.orange,
-                  child: Text(
-                    "Expanded flex: 3",
-                    style: TextStyle(fontSize: 25),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          // this takes you to screen 2
-          GestureDetector(
-            child: Padding(
-              child: Text('Click me for next page',
-                style: TextStyle(
-                  color: Colors.blue,
-                  fontSize: 20,
-                ),
-              ),
-              padding: EdgeInsets.all(30),
-            ),
-            onTap: () {
-              Navigator.of(context).pushNamed('/screen2');
-              },
-            ),
-          ],
         ),
       ),
     );
